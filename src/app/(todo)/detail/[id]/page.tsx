3176fcf729d9f8660b7fc232d5a507387fa43118
Jecheon Ryu <rjc1704@gmail.com>
2024-12-13 15:39:03 +0900

    metadata 추가

--- src/app/(todo)/detail/[id]/page.tsx
+++ src/app/(todo)/detail/[id]/page.tsx
@@ -1,9 +1,22 @@
 import { getTodo } from "@/api/todo";
 import HomeButton from "@/components/HomeButton";
+import { Metadata } from "next";
 
 type Props = {
   params: { id: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // fetch data
+  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`).then(
+    (res) => res.json(),
+  );
+
+  return {
+    title: data.title,
+    description: `${data.title}에 대한 내용`,
+  };
+}
 
 export default async function Detail({ params: { id } }: Props) {
   console.log("Detail 컴포넌트 렌더링");
--- src/app/(todo)/page.tsx
+++ src/app/(todo)/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import TodoForm from "./_components/TodoForm";
 import TodoList from "./_components/TodoList";
+
+export const metadata: Metadata = {
+  title: "투두홈",
+};
 
 export default function Home() {
   return (
